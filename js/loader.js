00fc2e845707b27684c4e8daa22fc0af75a317c7
JonathanBarrera07 <89654553+JonathanBarrera07@users.noreply.github.com>
2023-11-18 19:24:11 -0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,11 @@
-// Mostrar el loader al cargar la página
+
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("loader").style.display = "block";
-    document.getElementById("content").style.display = "none"; // Ocultar contenido mientras se carga
-  });
-  
-  // Ocultar el loader y mostrar el contenido cuando la página haya cargado completamente
-  window.addEventListener("load", function() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("content").style.display = "block";
-  });
-  
+  document.getElementById("loader").style.display = "flex";
+  document.getElementById("content").style.display = "none"; // Ocultar contenido mientras se carga
+});
+
+
+window.addEventListener("load", function() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("content").style.display = "block";
+});
